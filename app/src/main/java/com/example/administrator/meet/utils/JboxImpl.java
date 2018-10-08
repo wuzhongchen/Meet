af0522285f56c42c97c082ccb2bd315d1cca30aa
WuZhongCheng <944427937@qq.com>
2018-10-08 11:26:29 +0800

    third commit

--- app/src/main/java/com/example/administrator/meet/utils/JboxImpl.java
+++ app/src/main/java/com/example/administrator/meet/utils/JboxImpl.java
@@ -25,15 +25,15 @@ public class JboxImpl {
     //公式运算频率
     private float dt = 1f/30f;
 
-    private int mVelocityIterations =5; //每一帧迭代次数
+    private int mVelocityIterations = 5; //每一帧迭代次数
     private int mPositionIterations = 20;//迭代频率
 
     private float mWidth,mHeight;
 
-    private float mDesity=0.5f;
+    private float mDesity = 0.5f;
 
     //和界面的映射比例
-    private float mRatio=50;
+    private float mRatio = 50;
 
     private final Random mRandom = new Random();
 
@@ -43,29 +43,26 @@ public class JboxImpl {
     }
 
     //传宽度和高度
-    public void setWorldSize(int Width,int Height){
+    public void setWorldSize(int Width,int Height) {
         //映射
-        mWidth=Width;
-        mHeight=Height;
+        mWidth = Width;
+        mHeight = Height;
     }
 
-    public void startWorld(){
-
-        if(mWorld!=null){
-            Log.e("JboxImpl","启动模拟世界运动");
+    public void startWorld() {
+        if(mWorld != null) {
+//            Log.e("JboxImpl","启动模拟世界运动");
             mWorld.step(dt,mVelocityIterations,mPositionIterations);
         }
     }
 
-    public void createWorld(){
-
-        if(mWorld ==null){
+    public void createWorld() {
+        if(mWorld == null) {
             mWorld = new World(new Vec2(0,10.0f));
             update_Vertical_Bounds();
             update_Horizontal_Bounds();
             Log.e("JboxImpl","开始创建World");
-        }
-        else{
+        } else {
             Log.e("JboxImpl","World已经存在");
         }
     }
@@ -73,12 +70,10 @@ public class JboxImpl {
 
     //创建上下边界
     private void update_Vertical_Bounds() {
-
         BodyDef bodyDef = new BodyDef(); //定义的形状
-
         //BodyType.DYNAMIC是运动的，不受限制
         //STATIC 刚体的边界不能运动
-        bodyDef.type= BodyType.STATIC;
+        bodyDef.type = BodyType.STATIC;
 
         PolygonShape box = new PolygonShape();//形状确定为矩形
         float boxWidth = switchPositionToBody(mWidth);
@@ -87,8 +82,8 @@ public class JboxImpl {
 
 
         FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape=box;
-        fixtureDef.density=mDesity;
+        fixtureDef.shape = box;
+        fixtureDef.density = mDesity;
         fixtureDef.friction = 0.8f; //摩擦系数
         fixtureDef.restitution = 0.55f;//补偿系统
 
@@ -102,7 +97,6 @@ public class JboxImpl {
         Body topBody = mWorld.createBody(bodyDef);
         topBody.createFixture(fixtureDef);
         Log.e("updateVerticalBounds","创建顶部模拟世界");
-
     }
 
 
@@ -112,7 +106,7 @@ public class JboxImpl {
         BodyDef bodyDef = new BodyDef();
 
         //1.指定类型
-        bodyDef.type= BodyType.STATIC;
+        bodyDef.type = BodyType.STATIC;
 
         //2.形状设置为矩形
         PolygonShape box = new PolygonShape();
@@ -121,8 +115,8 @@ public class JboxImpl {
         box.setAsBox(boxWidth,boxHeight);
 
         FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape=box;
-        fixtureDef.density=mDesity;
+        fixtureDef.shape = box;
+        fixtureDef.density = mDesity;
         fixtureDef.friction = 0.8f; //摩擦系数
         fixtureDef.restitution = 0.55f;//补偿系统
 
@@ -145,18 +139,16 @@ public class JboxImpl {
         BodyDef bodyDef = new BodyDef();
         bodyDef.type  = BodyType.DYNAMIC;
 
-        Shape shape =null;
+        Shape shape = null;
 
         Boolean isCirCle = (Boolean) view.getTag(R.id.dn_view_circle_tag);
 
         if(isCirCle!=null&&isCirCle) {
-
             CircleShape box = new CircleShape();
             box.setRadius(switchPositionToBody((view.getWidth() / 2)));
             shape=box;
             Log.e("CreateBody","刚体形状设置为圆形");
-        }else
-        {
+        } else {
             return;
         }
 
@@ -166,8 +158,8 @@ public class JboxImpl {
         );
 
         FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape=shape;
-        fixtureDef.density=mDesity;
+        fixtureDef.shape = shape;
+        fixtureDef.density = mDesity;
         fixtureDef.friction = 0.8f;//摩擦系数
         fixtureDef.restitution = 0.0000000000001f; //补偿系统
 
--- app/src/main/java/com/example/administrator/meet/view/MyCountTimer.java
+++
@@ -1,4 +0,0 @@
-package com.example.administrator.meet.view;
-
-public class MyCountTimer {
-}
